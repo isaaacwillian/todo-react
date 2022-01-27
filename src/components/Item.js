284ba8eba1006a7bc9ece830e65be58f294b1c0d
Isaac Willian <isaaacwillian@gmail.com>
2022-01-26 22:05:31 -0300

    Consertado uns bugs do LocalStorage

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,15 +1,16 @@
-let items = JSON.parse(localStorage.getItem('savedItems'));
+let items = JSON.parse(localStorage.getItem("savedItems"));
 let lastId = 0;
 
-if(items.length !== 0){
+if (items) {
+  if (items.length !== 0) {
     lastId = ++items.slice(-1)[0].id;
+  }
 }
 
-function Item (text){
-
-    this.id = lastId++
-    this.text = text;
-    this.done = false;
+function Item(text) {
+  this.id = lastId++;
+  this.text = text;
+  this.done = false;
 }
 
 export default Item;
